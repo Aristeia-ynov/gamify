2a73d143295bd77954ab064350515015b2cf1ec4
Mouhcine <mouhcine.mesmouki7@gmail.com>
2022-06-21 23:52:48 +0100

    Update scripts.php

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,6 +1,3 @@
 <script src="assets/js/main.js"></script>
-<<<<<<< HEAD
 <!--<script src="assests/js/vendor/swiper.js"></script>-->
-=======
 <!--<script src="assests/js/vendor/swiper.js"></script>-->
->>>>>>> main
