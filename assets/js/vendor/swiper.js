2e65a9a9566e51b1ce7fa55062dbe4dcf5a76a31
Mouhcine <mouhcine.mesmouki7@gmail.com>
2022-06-21 23:57:09 +0100

    Update swiper.js

--- assets/js/vendor/swiper.js
+++ assets/js/vendor/swiper.js
@@ -3979,8 +3979,5 @@ Object.keys(prototypes).forEach((e => {
 })), Swiper.use([Resize, Observer]);
 const modules = [Virtual, Keyboard, Mousewheel, Navigation, Pagination, Scrollbar, Parallax, Zoom, Lazy, Controller, A11y, History, HashNavigation, Autoplay, Thumb, freeMode, Grid, Manipulation, EffectFade, EffectCube, EffectFlip, EffectCoverflow, EffectCreative, EffectCards];
 Swiper.use(modules);
-<<<<<<< HEAD
 export {Swiper, Swiper as default};
-=======
-export {Swiper, Swiper as default};
->>>>>>> main
+
